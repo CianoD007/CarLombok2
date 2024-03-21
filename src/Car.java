5fd1e40bcc8210327b319a0ac2c8f2946cb73b95
CianoD007 <161584275+CianoD007@users.noreply.github.com>
2024-03-21 13:22:50 +0100

    fix

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,9 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
+@AllArgsConstructor
 @EqualsAndHashCode
 class Car {
     private String model;
@@ -14,28 +16,12 @@ class Car {
 
 class CarDemo {
     public static void main(String[] args) {
-        // Creating Car objects using the default constructor provided by Lombok
-        Car car1 = new Car(); // This will create a new Car object with default values for fields
-        car1.setModel("Corolla");
-        car1.setYear(2022);
-        car1.setColor("Blue");
-        car1.setBrand("Toyota");
-        car1.setPrice(25000);
-        car1.setQuantity(10);
+        // Creating Car objects using the constructor provided by Lombok
+        Car car1 = new Car("Corolla", 2022, "Blue", "Toyota", 25000, 10);
+        Car car2 = new Car("Camry", 2021, "Red", "Toyota", 30000, 15);
 
-        // Printing the details of car1 using toString() method
+        // Printing the details of car1 and car2
         System.out.println(car1.toString());
-
-        // Creating another Car object using the default constructor provided by Lombok
-        Car car2 = new Car(); // This will create another Car object with default values for fields
-        car2.setModel("Camry");
-        car2.setYear(2021);
-        car2.setColor("Red");
-        car2.setBrand("Toyota");
-        car2.setPrice(30000);
-        car2.setQuantity(15);
-
-        // Printing the details of car2 using toString() method
         System.out.println(car2.toString());
 
         // Comparing two Car objects using equals and hashCode
