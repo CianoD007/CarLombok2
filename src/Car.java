2aa023d89f02c6d3e67a6ea25a25cf79cb5c9da8
CianoD007 <161584275+CianoD007@users.noreply.github.com>
2024-03-21 15:00:24 +0100

    fixed

--- src/Car.java
+++ src/Car.java
@@ -1,36 +1,32 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode(exclude= {"model", "year", "color", "price", "quantity"})
+
 class Car {
     private String model;
-    private int year;
-    private String color;
     private String brand;
-    private int price;
+    private int year;
+    private double price;
+    private String color;
     private int quantity;
-
-    @Override
-    public boolean equals(Object obj) {
-        return true; // Always return true for equality check
-    }
-
-    @Override
-    public int hashCode() {
-        return 123; // Return the same constant hash code value for all objects
-    }
 }
 
 class CarDemo {
     public static void main(String[] args) {
-        // Creating Car objects using the constructor provided by Lombok
-        Car car1 = new Car("Corolla", 2022, "Blue", "Toyota", 25000, 10);
-        Car car2 = new Car("Toyota", 2021, "Red", "Toyota", 30000, 15);
+        // Creating car objects using the constructor provided by Lombok
+        Car car1 = new Car("Corolla", "Toyota", 2022, 25000.0, "Red", 10);
+        Car car2 = new Car("Camry", "Toyota", 2021, 30000.0, "Blue", 15);
+
 
         // Printing the details of car1 and car2
         System.out.println(car1.toString());
         System.out.println(car2.toString());
+        System.out.println(car1.getColor());
+        car1.setColor("Black");
 
         // Comparing two Car objects using equals and hashCode
         System.out.println("Are car1 and car2 equal? " + car1.equals(car2));
